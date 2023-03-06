2b5e59d3446bc819a0f591a7c28907a13ad8f957
Данила Тяпкин <dtapkin6@gmail.com>
2023-03-06 17:29:57 +0300

    Тоже исправил

--- app/src/main/java/com/example/clickerbro/MainActivity.java
+++ app/src/main/java/com/example/clickerbro/MainActivity.java
@@ -18,44 +18,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Log.d(TAG, "onStart");
-        resetUI();
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Log.d(TAG, "onResume");
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.d(TAG, "onPause");
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Log.d(TAG, "onStop");
-    }
-
-    @Override
-    protected void onRestart(){
-        super.onRestart();
-        Log.d(TAG, "onRestart");
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.d(TAG, "onDestroy");
-    }
-
     public void onClickBtnAddBro(View view)
     {
         counter++;
