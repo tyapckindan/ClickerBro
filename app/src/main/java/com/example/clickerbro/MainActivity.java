c84e7517ed2303ee2136b47a9cc3a1691556d3fa
Данила Тяпкин <dtapkin6@gmail.com>
2023-11-17 11:58:29 +0300

    Кликер шахтёра

--- app/src/main/java/com/example/clickerbro/MainActivity.java
+++ app/src/main/java/com/example/clickerbro/MainActivity.java
@@ -1,49 +1,99 @@
 package com.example.clickerbro;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "StartActivity";
+    static String KEY_COUNT_GOLD = "COUNT_GOLD";
+    int count_gold = 0; // кол-во золота
+    static String KEY_ADD_GOLD = "ADD_GOLD";
+    int add_gold = 1; // буст золота
+    static String KEY_RAB_MESTO = "RAB_MESTO";
+    int rab_mesto = 10; // рабочие места
+    static String KEY_COUNT_SHAHTA = "COUNT_SHAHTA";
+    int count_shahta = 1; // Количество шахт
+    static String KEY_DOP_ADD_GOLD = "DOP_ADD_GOLD";
+    int dop_add_gold = 0; // Золото, что добывают рабы
+    TextView goldview;
+    CountDownTimer timer;
 
-    private Integer counter = 0;
+    ActivityResultLauncher<Intent> ShahtaLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    // все ли хорошо при получении данных из дочерней активити?
+                    if(result.getResultCode() == Activity.RESULT_OK)
+                    {
+                        //получить данные
+                        Intent intent = result.getData();
+                        count_gold = intent.getIntExtra("COUNT_GOLD", 0);
+                        goldview = findViewById(R.id.txt_score_gold);
+                        goldview.setText(count_gold + " золота");
+                        add_gold = intent.getIntExtra("ADD_GOLD", 0);
+                        rab_mesto = intent.getIntExtra("RAB_MESTO", 0);
+                        count_shahta = intent.getIntExtra("COUNT_SHAHTA", 0);
+                        dop_add_gold = intent.getIntExtra("DOP_ADD_GOLD", 0);
+                        assert intent != null;
+                    }
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
-    }
-    public void onClickBtnAddBro(View view)
-    {
-        counter++;
-        TextView counterView = findViewById(R.id.txt_counter);
-        counterView.setText(counter.toString());
-    }
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt("counter", counter);
-        Log.d(TAG, "onSaveInstanceState");
+        timer = new CountDownTimer(86400000, 2000) {
+            @Override
+            public void onTick(long l) {
+                if (count_gold < 5000) {
+                    count_gold += dop_add_gold;
+                    goldview = findViewById(R.id.txt_score_gold);
+                    goldview.setText(count_gold + " золота");
+                }
+            }
+            @Override
+            public void onFinish() {
+
+                    }
+                }.start();
+            }
+
+    // Кнопка добыть золото
+    public void AddGold(View view) {
+        if (count_gold < 5000) {
+            count_gold += add_gold;
+            goldview = findViewById(R.id.txt_score_gold);
+            goldview.setText(count_gold + " золота");
+        }
+        else
+        {
+            goldview.setText("Победа! Наслаждайтесь своим рабным гаремом...");
+        }
     }
 
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        if (savedInstanceState != null &&
-                savedInstanceState.containsKey("counter")) {
-            counter = savedInstanceState.getInt("counter");
-            TextView counterView = findViewById(R.id.txt_counter);
-            counterView.setText(counter.toString());
-        }
-        Log.d(TAG, "onRestoreInstanceState");
+    // Кнопка магазин, переход на активити магазина
+    public void Shop(View view) {
+        Intent intent = new Intent(
+                MainActivity.this,
+                ShopActivity.class);
+
+        intent.putExtra(KEY_COUNT_GOLD, count_gold);
+        intent.putExtra(KEY_ADD_GOLD, add_gold);
+        intent.putExtra(KEY_RAB_MESTO, rab_mesto);
+        intent.putExtra(KEY_COUNT_SHAHTA, count_shahta);
+        intent.putExtra(KEY_DOP_ADD_GOLD, dop_add_gold);
+        ShahtaLauncher.launch(intent);
     }
-    private void resetUI() {
-        ((TextView) findViewById(R.id.txt_counter)).setText(counter.toString());
-        Log.d(TAG, "resetUI");
-    }
-    }
+}
---
+++ app/src/main/java/com/example/clickerbro/ShopActivity.java
@@ -0,0 +1,118 @@
+package com.example.clickerbro;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShopActivity extends AppCompatActivity {
+
+    TextView goldview, message;
+    // count_gold - кол-во золота, add_gold - прибавка золота за клик, rab_mesto - рабочие места,
+    // count_shahta - кол-во шахт, dop_add_gold - кол-во золота, которое добывают рабы
+    Integer count_gold = 0, add_gold = 0, rab_mesto = 10, count_shahta = 1, dop_add_gold = 0;
+    CountDownTimer timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_shop);
+
+        timer = new CountDownTimer(86400000, 2000) {
+            @Override
+            public void onTick(long l) {
+                if (count_gold < 5000) {
+                    count_gold += dop_add_gold;
+                    goldview = findViewById(R.id.txt_score);
+                    goldview.setText(count_gold + " золота");
+                }
+            }
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+
+        // получить данные из родительской активити
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            count_gold = extras.getInt(MainActivity.KEY_COUNT_GOLD);
+            add_gold = extras.getInt(MainActivity.KEY_ADD_GOLD);
+            rab_mesto = extras.getInt(MainActivity.KEY_RAB_MESTO );
+            count_shahta = extras.getInt(MainActivity.KEY_COUNT_SHAHTA);
+            dop_add_gold = extras.getInt(MainActivity.KEY_DOP_ADD_GOLD);
+        }
+
+        goldview = findViewById(R.id.txt_score);
+        message = findViewById(R.id.txt_message);
+        goldview.setText(count_gold.toString() + " золота");
+    }
+
+    // Кнопка улучшения кирки
+    public void btn_click_up_kirka (View view) {
+        int price = 10;
+        // Если золота больше чем цена, то улучшаем, иначе недостаточно золота
+        if (count_gold >= price){
+            count_gold -= price;
+
+            add_gold += 1;
+
+            goldview = findViewById(R.id.txt_score);
+            goldview.setText(count_gold + " золота");
+            message.setText("Вы улучшили свой инструмент!");
+        }
+        else {
+            message.setText("У вас недостаточно золота!");
+        }
+    }
+    // Кнопка покупки шахты
+    public void btn_click_bought_shahta (View view) {
+        int price = 500;
+        // Если золота больше чем цена, то покупаем, иначе недостаточно золота
+        if (count_gold >= price){
+            count_gold -= price;
+            count_shahta += 1;
+            rab_mesto += 10;
+
+            goldview = findViewById(R.id.txt_score);
+            goldview.setText(count_gold + " золота");
+            message.setText("Вы купили новую шахту!");
+        }
+        else {
+            message.setText("У вас недостаточно золота!");
+        }
+    }
+    // Кнопка покупки раба
+    public void btn_click_bought_rab (View view) {
+        int price = 20;
+        // Если золота больше чем цена и есть место, то покупаем, иначе недостаточно золота, либо не хватает места
+        if (count_gold >= price && rab_mesto >= 1){
+            count_gold -= price;
+            rab_mesto -= 1;
+            dop_add_gold += 1;
+
+            goldview = findViewById(R.id.txt_score);
+            goldview.setText(count_gold + " золота");
+            message.setText("Вы купили раба!");
+        }
+        else {
+            message.setText("У вас недостаточно золота, либо не хватает места!");
+        }
+    }
+
+    // Кнопка выхода - возврат на родительскую активити
+    public void btn_click_exit_shop (View view) {
+
+        Intent data = new Intent();
+        data.putExtra(MainActivity.KEY_COUNT_GOLD,count_gold);
+        data.putExtra(MainActivity.KEY_ADD_GOLD, add_gold);
+        data.putExtra(MainActivity.KEY_RAB_MESTO,rab_mesto);
+        data.putExtra(MainActivity.KEY_COUNT_SHAHTA, count_shahta);
+        data.putExtra(MainActivity.KEY_DOP_ADD_GOLD, dop_add_gold);
+        setResult(RESULT_OK, data);
+        finish();
+    }
+}
